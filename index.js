5957afe07fcdef78a4be0c009d831e4def76fa2a
preciousniphemi <preciousoluwanifemi1999@gmail.com>
2024-05-14 23:47:28 +0100

    chore: add generate attio endpoint

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const { createProxyMiddleware } = require("http-proxy-middleware");
 const cors = require("cors");
-
+const axios = require("axios");
 const app = express();
+const https = require("https");
 
 // Middleware to log all incoming requests
 app.use((req, res, next) => {
@@ -24,31 +25,88 @@ app.use(
   })
 );
 
+// Add this line to use express.json middleware
+app.use(express.json());
+
 // Proxy configuration for /deepgram-transcribe
 const proxyOptions = {
   target: "https://api.runloop.ai",
   changeOrigin: true,
   pathRewrite: {
     "^/deepgram-transcribe":
-      "/v1/projects/1714679056570/functions/deepgram_transcription/invoke_blocking",
+      "/v1/projects/1714679056570/functions/deepgram_transcription/invoke_nonblocking",
   },
+};
+
+const anotherProxyOption = {
+  target: "https://api.runloop.ai",
+  changeOrigin: true,
+  pathRewrite: {
+    "^/generate-attio":
+      "/v1/projects/1714679056570/functions/generate_attio/invoke_nonblocking",
+  },
+  proxyTimeout: 900000,
 };
 
 // Apply the proxy middleware for POST requests to /deepgram-transcribe
 app.post("/deepgram-transcribe", createProxyMiddleware(proxyOptions));
 
-// New proxy configuration for the additional endpoint
-const soapProxyOptions = {
-  target: "https://api.runloop.ai", // Replace with the target host for the new endpoint
-  changeOrigin: true,
-  pathRewrite: {
-    "^/scheduler":
-      "/v1/projects/1714679056570/functions/generate_code/invoke_blocking", // Replace with the actual path on the target server
-  },
-};
+app.post("/generate_attio", (req, res) => {
+  console.log("THE REQUEST IS", req.body);
+  axios({
+    method: "post",
+    url: `https://api.runloop.ai/v1/projects/1714679056570/functions/generate_attio/invoke_nonblocking`,
+    data: req.body,
+    headers: {
+      "Content-Type": req.headers["content-type"],
+      Authorization: req.headers["authorization"],
+      // include other necessary headers here
+    },
+  })
+    .then((response) => {
+      console.log("response", "response", response.data);
+      res.status(200).send("Request sent");
+    })
+    .catch((error) => {
+      console.log("THE ERROR IS", error);
+      res.status(500).send("Error sending request");
+    });
+});
 
-// Apply the proxy middleware for POST requests to /another-endpoint
-app.post("/scheduler", createProxyMiddleware(soapProxyOptions));
+// app.post("/scheduler", (req, res) => {
+//   const data = JSON.stringify(req.body);
+
+//   const options = {
+//     hostname: "api.runloop.ai",
+//     path: "/v1/projects/1714679056570/functions/transcription_scheduler/invoke_nonblocking",
+//     method: "POST",
+//     rejectUnauthorized: false,
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Content-Length": Buffer.byteLength(data),
+//       ...req.headers, // Forward all incoming headers
+//     },
+//   };
+
+//   // Create the request to the external server
+//   const proxyReq = https.request(options, (proxyRes) => {
+//     // Optionally log the status code for debugging
+//     console.log(`RESPONSE: ${proxyRes}`);
+
+//     console.log(`STATUS: ${proxyRes.statusCode}`);
+//   });
+
+//   proxyReq.on("error", (e) => {
+//     console.error(`Problem with request: ${e.message}`);
+//   });
+
+//   // Write data to request body and end the request
+//   proxyReq.write(data);
+//   proxyReq.end();
+
+//   // Immediately respond to the client without waiting for the external request to complete
+//   res.status(202).send("Request is being processed");
+// });
 
 // Start the server
 const PORT = process.env.PORT || 8080;
